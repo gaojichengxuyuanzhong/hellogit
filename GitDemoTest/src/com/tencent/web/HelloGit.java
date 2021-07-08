918e571c784919e07789c3b2d048f127d43a14ed
xiaoming <xiaoming@qq.com>
2021-07-08 15:07:06 +0800

    小明第一次提交的项目

---
+++ GitDemoTest/src/com/tencent/web/HelloGit.java
@@ -0,0 +1,14 @@
+package com.tencent.web;
+
+public class HelloGit {
+	
+	/*
+	 * public static void main(String[] args) { System.out.println("Hello,git!!!");
+	 * }
+	 */
+	
+//	这是小明写的一个方法
+	public void addUser() {
+		
+	}
+}
